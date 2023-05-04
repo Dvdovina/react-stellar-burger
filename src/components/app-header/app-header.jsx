91eaa3341b113de29d21617fe01d246a3b89a057
Daria Vdovina <vdovinadn@gmail.com>
2023-05-04 21:27:36 +0300

    fix: header elements are now links

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -11,20 +11,26 @@ function AppHeader() {
             <nav className={headerStyles.navigation}>
                 <ul className={headerStyles.list}>
                     <li className={`${headerStyles.item} pb-4 pt-4 pl-5 pr-5`}>
-                        <BurgerIcon />
-                        <p className="text text_type_main-default">Конструктор</p>
+                        <a href="#" className={headerStyles.link}>
+                            <BurgerIcon type="primary" />
+                            <p className="text text_type_main-default">Конструктор</p>
+                        </a>
                     </li>
                     <li className={`${headerStyles.item} pb-4 pt-4 pl-5`}>
-                        <ListIcon type="secondary" />
-                        <p className="text text_type_main-default text_color_inactive">Лента заказов</p>
+                        <a href="#" className={headerStyles.link}>
+                            <ListIcon type="secondary" />
+                            <p className="text text_type_main-default text_color_inactive">Лента заказов</p>
+                        </a>
                     </li>
                     <li className={`${headerStyles.item} pb-4 pt-4 pl-5 pr-5`}>
-                        <ProfileIcon type="secondary" />
-                        <p className="text text_type_main-default text_color_inactive">Личный кабинет</p>
+                        <a href="#" className={headerStyles.link}>
+                            <ProfileIcon type="secondary" />
+                            <p className="text text_type_main-default text_color_inactive">Личный кабинет</p>
+                        </a>
                     </li>
                 </ul>
             </nav>
-        </header>
+        </header >
     )
 }
 
