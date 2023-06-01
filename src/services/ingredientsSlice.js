48e4eb896a203f4c0d99003a33eefe6dc949fc63
Daria Vdovina <vdovinadn@gmail.com>
2023-06-01 19:20:58 +0300

    build: ingredientsSlice, createAsyncThunk

--- src/services/ingredientsSlice.js
+++ src/services/ingredientsSlice.js
@@ -1,17 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { getData } from '../utils/api'
+
+
+export const fetchIngredients = createAsyncThunk(
+  'ingredients/fetchIngredients',
+  async () => {
+    const response = await getData()
+    return response.data
+  }
+)
 
 const initialState = {
   ingredients: [],
   request: '',
   error: null,
-
 };
 
 export const ingredientsSlice = createSlice({
   name: 'ingredients',
   initialState,
   reducers: {},
-  extraReducers: (builder) => { }
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchIngredients.pending, (state) => {
+        state.request = true
+        state.error = null
+      })
+      .addCase(fetchIngredients.fulfilled, (state, action) => {
+        state.ingredients = action.payload
+        state.request = false
+      })
+      .addCase(fetchIngredients.rejected, (state, action) => {
+        state.request = false
+        state.error = action.payload
+      });
+  }
 })
 
 export default ingredientsSlice.reducer
