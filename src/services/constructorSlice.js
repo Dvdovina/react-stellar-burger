f3062133a17a2de08a1508e7e1651b1d6763e76f
Daria Vdovina <vdovinadn@gmail.com>
2023-06-01 19:48:54 +0300

    build: constructorSlice, store

--- src/services/constructorSlice.js
+++ src/services/constructorSlice.js
@@ -1,11 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    
-  };
+  ingredients: [],
+  bun: null
+};
 
-  export const constructorSlice = createSlice({
+export const constructorSlice = createSlice({
+  name: 'userBurgerIngredients',
+  initialState,
+  reducers: {
+    addIngredient(state, { item }) {
+      if (item.type === 'bun') {
+        state.bun = item
+      } else {
+        state.ingredients = [...state.ingredients, item]
+      }
+    },
+    deleteIngredient(state, { item: id }) {
+      state.ingredients = state.ingredients.filter(ingredient => ingredient._id !== id)
+    },
+    refreshIngredients(state, { items }) {
+      state.ingredients = items
+    }
+  }
+})
 
 
-
-  })
+export default constructorSlice.reducer;
+export const { addIngredient, deleteIngredient, refreshIngredients } = constructorSlice.actions
--- src/services/store.js
+++ src/services/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import currentIngredientSlice from "./currentIngredientSlice"
 import ingredientsSlice from './ingredientsSlice'
+import constructorSlice from './constructorSlice'
 
 export const store = configureStore({
     reducer: {
         ingredients: ingredientsSlice,
-        currentIngredient: currentIngredientSlice
+        currentIngredient: currentIngredientSlice,
+        userBurgerIngredients: constructorSlice
     },
 })
