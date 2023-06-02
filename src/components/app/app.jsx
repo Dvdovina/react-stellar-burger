89ce4228f32ac06577cbb12f87e28a9df3c39ecf
Daria Vdovina <vdovinadn@gmail.com>
2023-06-02 19:01:00 +0300

    refactor: store in App component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,15 @@
 import styles from "./app.module.css";
-// import { data } from "../../utils/data"; исходные данные без апи
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { getData } from "../../utils/api";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchIngredients } from "../../services/ingredientsSlice";
 
 
 function App() {
   const dispatch = useDispatch();
-  const { ingredients, ingredientsStatus, ingredientsError } = useSelector(
+  const { ingredients } = useSelector(
     (store) => store.ingredients,
   );
 
@@ -19,37 +17,13 @@ function App() {
     dispatch(fetchIngredients());
   }, [dispatch]);
 
-  // const [burgerData, setBurgerData] = useState([]);
-  // const [error, setError] = useState(false);
-
-  // const getBurgerData = () => {
-  //   getData()
-  //     .then((res) => {
-  //       setBurgerData(res.data);
-  //     })
-  //     .catch(() => {
-  //       setError(true);
-  //     })
-  // };
-
-  // useEffect(() => {
-  //   getBurgerData();
-  // }, []);
-
   return (
     <div className={`custom-scroll ${styles.app}`}>
-      {ingredientsError ? (
-        <span className={`${styles.error} text_type_main-medium`}>Ошибка загрузки данных.
-          Приносим свои извинения,попробуйте перезагрузить страницу.</span>
-      ) : (
-        <>
           <AppHeader />
           <main className={styles.main}>
             <BurgerIngredients ingredients={ingredients} />
             <BurgerConstructor ingredients={ingredients} />
           </main>
-        </>
-      )}
     </div>
   );
 }
