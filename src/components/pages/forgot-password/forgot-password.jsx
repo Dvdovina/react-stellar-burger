fd2e5a4bdc3e71d4a98d07c1a0329019f0a0fe56
Daria Vdovina <vdovinadn@gmail.com>
2023-06-17 23:45:44 +0300

    build: ForgotPassword component+styles

--- src/components/pages/forgot-password/forgot-password.jsx
+++ src/components/pages/forgot-password/forgot-password.jsx
@@ -1,13 +1,36 @@
 import forgotPasswordStyles from './forgot-password.module.css'
-
+import { Link } from "react-router-dom";
+import { useState, useRef } from 'react';
+import { Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
 
 function ForgotPassword() {
 
+    const [email, setEmail] = useState('');
+    const inputRef = useRef(null);
 
 
     return (
         <>
+            <section className={forgotPasswordStyles.section}>
+                <form className={forgotPasswordStyles.form}>
+                    <h2 className="text text_type_main-medium">Восстановление пароля</h2>
+                    <Input
+                        type={'email'}
+                        placeholder={'Укажите e-mail'}
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
+                        name={'email'}
+                        error={false}
+                        ref={inputRef}
+                        errorText={'Ошибка'}
+                    />
+                    <Button htmlType="submit" type="primary" size="large" disabled={!email}>Восстановить</Button>
+                </form>
+                <span className="text text_type_main-default text_color_inactive pt-20">
+                    Вспомнили пароль? <Link to="/login" className={forgotPasswordStyles.link}>Войти</Link>
+                </span>
+            </section>
         </>
     )
 }
