1f98aaaca438f7a68f2cca329792a34544760df8
Daria Vdovina <vdovinadn@gmail.com>
2023-06-19 21:58:06 +0300

    build: ErrorPage component+styles

--- src/components/pages/error-page/error-page.jsx
+++ src/components/pages/error-page/error-page.jsx
@@ -1,13 +1,26 @@
-import ErrorPageStyles from './error-page.module.css'
+import errorPageStyles from './error-page.module.css'
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useNavigate } from "react-router-dom";
 
 
 
 function ErrorPage() {
 
+    const navigate = useNavigate();
+
+    const returnToMain = () => {
+        navigate("/");
+    };
 
 
     return (
         <>
+            <section className={errorPageStyles.section}>
+                <h2 className="text text_type_main-medium">Упс! Такой страницы не существует...</h2>
+                <Button htmlType="button" type="primary" size="medium" onClick={returnToMain}>
+                    Вернуться на главную?
+                </Button>
+            </section>
         </>
     )
 }
