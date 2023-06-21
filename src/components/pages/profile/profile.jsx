e03f93a9900871f31d1488068fc5403b96eb65dd
Daria Vdovina <vdovinadn@gmail.com>
2023-06-21 18:24:36 +0300

    build: Profile component

--- src/components/pages/profile/profile.jsx
+++ src/components/pages/profile/profile.jsx
@@ -1,13 +1,75 @@
 import profileStyles from './profile.module.css'
-
+import { Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useState, useRef } from 'react';
+import { NavLink } from 'react-router-dom'
 
 
 function Profile() {
 
+    const inputRef = useRef(null);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
 
     return (
         <>
+            <section className={profileStyles.section}>
+                <nav className={profileStyles.menu}>
+                    <NavLink
+                        to={'/profile'}
+                        className={({ isActive }) => isActive ? `${profileStyles.link} text text_type_main-medium ${profileStyles.link_active}` :
+                            `${profileStyles.link} text text_type_main-medium text_color_inactive`}>
+                        Профиль
+                    </NavLink>
+                    <NavLink
+                        to={'/profile/orders'}
+                        className={({ isActive }) => isActive ? `${profileStyles.link} text text_type_main-medium ${profileStyles.link_active}` :
+                            `${profileStyles.link} text text_type_main-medium text_color_inactive`}>
+                        История заказов
+                    </NavLink>
+                    <NavLink
+                        to={'/logout'}
+                        className={({ isActive }) => isActive ? `${profileStyles.link} text text_type_main-medium ${profileStyles.link_active}` :
+                            `${profileStyles.link} text text_type_main-medium text_color_inactive`}>
+                        Выход
+                    </NavLink>
+                    <span className="text text_type_main-small text_color_inactive pt-20">
+                        В этом разделе вы можете<br /> изменить свои персональные данные
+                    </span>
+                </nav>
+                <form className={profileStyles.form}>
+                    <Input
+                        type={'text'}
+                        placeholder={'Имя'}
+                        onChange={e => setName(e.target.value)}
+                        value={name}
+                        name={'name'}
+                        error={false}
+                        ref={inputRef}
+                        errorText={'Ошибка'}
+                        icon="EditIcon"
+                    />
+                    <Input
+                        type={'email'}
+                        placeholder={'Логин'}
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
+                        name={'email'}
+                        error={false}
+                        ref={inputRef}
+                        errorText={'Ошибка'}
+                        icon="EditIcon"
+                    />
+                    <PasswordInput
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
+                        name={'password'}
+                        placeholder={'Пароль'}
+                        icon="EditIcon"
+                    />
+                </form>
+            </section>
         </>
     )
 }
