8b1ee3f95306e5e8d17c0170fdd623d116fead26
Daria Vdovina <vdovinadn@gmail.com>
2023-06-21 19:28:40 +0300

    build: NavLinks in header+refactor

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,32 +1,46 @@
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import headerStyles from './app-header.module.css';
+import { NavLink, useMatch, Link } from "react-router-dom";
 
 
 function AppHeader() {
+
+    const home = useMatch('/');
+    const list = useMatch('/profile/orders');
+    const profile = useMatch('/profile');
+
     return (
         <header className={headerStyles.header}>
             <div className={headerStyles.logo}>
-                <Logo />
+                <Link to="/">
+                    <Logo />
+                </Link>
             </div>
             <nav className={headerStyles.navigation}>
                 <ul className={headerStyles.list}>
                     <li className={`${headerStyles.item} pb-4 pt-4 pl-5 pr-5`}>
-                        <a href="#" className={headerStyles.link}>
-                            <BurgerIcon type="primary" />
-                            <p className="text text_type_main-default">Конструктор</p>
-                        </a>
+                        <NavLink end to={'/'}
+                            className={({ isActive }) => isActive ? `${headerStyles.link} text text_type_main-default ${headerStyles.link_active}` :
+                                `${headerStyles.link} text text_type_main-default text_color_inactive`}>
+                            <BurgerIcon type={home ? "primary" : "secondary"} />
+                            Конструктор
+                        </NavLink>
                     </li>
                     <li className={`${headerStyles.item} pb-4 pt-4 pl-5`}>
-                        <a href="#" className={headerStyles.link}>
-                            <ListIcon type="secondary" />
-                            <p className="text text_type_main-default text_color_inactive">Лента заказов</p>
-                        </a>
+                        <NavLink end to={'/profile/orders'}
+                            className={({ isActive }) => isActive ? `${headerStyles.link} text text_type_main-default ${headerStyles.link_active}` :
+                                `${headerStyles.link} text text_type_main-default text_color_inactive`}>
+                            <ListIcon type={list ? "primary" : "secondary"} />
+                            Лента заказов
+                        </NavLink>
                     </li>
                     <li className={`${headerStyles.item} pb-4 pt-4 pl-5 pr-5`}>
-                        <a href="#" className={headerStyles.link}>
-                            <ProfileIcon type="secondary" />
-                            <p className="text text_type_main-default text_color_inactive">Личный кабинет</p>
-                        </a>
+                        <NavLink end to={'/profile'}
+                            className={({ isActive }) => isActive ? `${headerStyles.link} text text_type_main-default ${headerStyles.link_active}` :
+                                `${headerStyles.link} text text_type_main-default text_color_inactive`}>
+                            <ProfileIcon type={profile ? "primary" : "secondary"} />
+                            Личный кабинет
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
