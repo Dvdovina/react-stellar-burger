1150bcfd29b63c0f164f5b37a7976e4c91c5f80a
Daria Vdovina <vdovinadn@gmail.com>
2023-07-10 06:30:13 +0300

    build: modal route pre-build

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,6 @@ import AppHeader from "../app-header/app-header";
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import ForgotPassword from "../pages/forgot-password/forgot-password";
 import Home from "../pages/home/home";
-import IngredientPage from "../pages/ingredient-page/ingredient-page";
 import Login from "../pages/login/login";
 import ErrorPage from "../pages/error-page/error-page";
 import Profile from "../pages/profile/profile";
@@ -13,11 +12,16 @@ import { getUser } from "../../services/userSlice";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Protected from "../protected-route-element/protected-route-element";
+import IngredientDetails from "../ingredient-details/ingredient-details";
+import Modal from "../modal/modal";
 
 
 function App() {
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const background = location.state && location.state.background;
 
   useEffect(() => {
     try {
@@ -29,13 +33,16 @@ function App() {
     }
   }, [])
 
+  const handleCloseModal = () => {
+    navigate(-1);
+  };
+
 
   return (
     <div className={`custom-scroll ${styles.app}`}>
       <AppHeader />
-      <Routes >
+      <Routes location={background || location} >
         <Route path='/' element={<Home />} />
-        <Route path='/ingredients/:id' element={<IngredientPage />} />
         <Route path='/profile' element={<Protected><Profile /></Protected>} />
         <Route path='/login' element={<Protected onlyUnAuth><Login /></Protected>} />
         <Route path='/register' element={<Protected onlyUnAuth><Register /></Protected>} />
@@ -43,6 +50,11 @@ function App() {
         <Route path='/reset-password' element={<Protected onlyUnAuth><ResetPassword /></Protected>} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
+      {background && (
+      <Routes>
+        <Route path='/ingredients/:id' element={<Modal onClose={handleCloseModal}><IngredientDetails /></Modal>} />
+      </Routes>
+        )}
     </div>
   );
 }
