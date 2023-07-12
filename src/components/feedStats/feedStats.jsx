3107d806efe821e00228dcc6cf7707cf35ccb2a4
Daria Vdovina <vdovinadn@gmail.com>
2023-07-12 16:11:51 +0300

    build: FeedStats component (test version)

--- src/components/feedStats/feedStats.jsx
+++ src/components/feedStats/feedStats.jsx
@@ -1,23 +1,36 @@
 import feedStatsStyles from './feedStats.module.css'
 
-
-
-
-
-
 function FeedStats() {
-
-
-
-
-
    return (
       <section className={feedStatsStyles.section}>
-
-
-
-
-
+         <div className={feedStatsStyles.status}>
+            <div className={feedStatsStyles.status_box}>
+               <p className="text text_type_main-medium pb-6">Готовы:</p>
+               <ul className={feedStatsStyles.orders_list} >
+                  <li className={`${feedStatsStyles.number, feedStatsStyles.done} text text_type_digits-default`}>012345</li>
+                  <li className={`${feedStatsStyles.number, feedStatsStyles.done} text text_type_digits-default`}>012345</li>
+                  <li className={`${feedStatsStyles.number, feedStatsStyles.done} text text_type_digits-default`}>012345</li>
+                  <li className={`${feedStatsStyles.number, feedStatsStyles.done} text text_type_digits-default`}>012345</li>
+                  <li className={`${feedStatsStyles.number, feedStatsStyles.done} text text_type_digits-default`}>012345</li>
+               </ul>
+            </div>
+            <div className={feedStatsStyles.status_box}>
+               <p className="text text_type_main-medium pb-6">В работе:</p>
+               <ul className={feedStatsStyles.orders_list}>
+                  <li className={`${feedStatsStyles.number} text text_type_digits-default`}>012345</li>
+                  <li className={`${feedStatsStyles.number} text text_type_digits-default`}>012345</li>
+                  <li className={`${feedStatsStyles.number} text text_type_digits-default`}>012345</li>
+               </ul>
+            </div>
+         </div>
+         <div className={feedStatsStyles.statistics}>
+            <p className="text text_type_main-medium">Выполнено за все время:</p>
+            <span className={`${feedStatsStyles.shadow} text text_type_digits-large`}>25673</span>
+         </div>
+         <div className={feedStatsStyles.statistics}>
+            <p className="text text_type_main-medium">Выполнено за сегодня:</p>
+            <span className={`${feedStatsStyles.shadow} text text_type_digits-large`}>423</span>
+         </div>
       </section>
    )
 }
