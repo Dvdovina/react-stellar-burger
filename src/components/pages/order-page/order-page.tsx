0545a93523c4217a4a0a7d5fea9d70c9be97c54b
Daria Vdovina <vdovinadn@gmail.com>
2023-10-16 19:19:04 +0300

    refactor: TS in order-page

--- src/components/pages/order-page/order-page.tsx
+++ src/components/pages/order-page/order-page.tsx
@@ -2,26 +2,27 @@ import orderStyles from './order-page.module.css'
 import OrderModal from '../../order-modal/order-modal'
 import { wsConnect, wsDisconnect } from '../../../services/actions/wsActions'
 import { WS_FEED_URL } from '../../../utils/api'
-import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useParams } from 'react-router'
+import { useAppDispatch, useAppSelector } from '../../../hooks/useForm'
+import { TOrder } from '../../../utils/common-types';
 
 
 function OrderPage() {
 
-    const dispatch = useDispatch()
+    const dispatch = useAppDispatch()
     const { id } = useParams();
 
     useEffect(() => {
         dispatch(wsConnect(WS_FEED_URL));
-        return () => dispatch(wsDisconnect());
+        return () => {dispatch(wsDisconnect())};
     }, []);
 
-    const { orders } = useSelector(
+    const { orders } = useAppSelector(
         (store) => store.feed,
     );
 
-    const order = orders.find((order) => order._id === id);
+    const order = orders.find((order: TOrder) => order._id === id);
 
     return (
         <section className={orderStyles.section}>
