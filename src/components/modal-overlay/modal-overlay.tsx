59a9cb934030db9a205f56c5a11777c1d8f712b2
Daria Vdovina <vdovinadn@gmail.com>
2023-10-18 21:54:59 +0300

    refactor: TS in modal-overlay

--- src/components/modal-overlay/modal-overlay.tsx
+++ src/components/modal-overlay/modal-overlay.tsx
@@ -1,14 +1,14 @@
 import overlayStyles from "./modal-overlay.module.css"
-import PropTypes from "prop-types"
+import { FC } from "react"
 
-function ModalOverlay({ onClose }) {
+interface IModalOverlayProps {
+    onClose: () => void;
+}
+
+const ModalOverlay: FC<IModalOverlayProps> = ({ onClose }) => {
     return (
         <div onClick={onClose} className={overlayStyles.overlay}></div>
     )
-}
-
-ModalOverlay.propTypes = {
-    onClose: PropTypes.func.isRequired
 }
 
 export default ModalOverlay
