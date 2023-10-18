03b6c221b55d6b1cc057f84d88bc20f5ea594587
Daria Vdovina <vdovinadn@gmail.com>
2023-10-19 02:05:17 +0300

    refactor: TS in ingredient-details

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,20 +1,21 @@
 import detailsStyles from './ingredient-details.module.css';
-import { useSelector} from "react-redux";
 import { useParams } from "react-router-dom";
+import { useAppSelector } from '../../hooks/useForm';
+import { TIngredient } from '../../utils/common-types';
 
-
+interface Params {
+    id: string;
+  }
 
 function IngredientDetails() {
 
-    const { id } = useParams();
+    const { id } = useParams<keyof Params>() as Params;
 
-    const { currentIngredient } = useSelector((state) => state.currentIngredient);
-
-    const { ingredients } = useSelector((state) => state.ingredients)
+    const { ingredients } = useAppSelector((state) => state.ingredients)
     if (!ingredients.length) return null
     
     const { proteins, fat, carbohydrates, calories, name, image_large } =
-    ingredients.find(({ _id }) => _id === id)
+    ingredients?.find(({ _id }) => _id === id) as TIngredient
 
     return (
             <div className={detailsStyles.container}>
