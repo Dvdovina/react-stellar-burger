b2cbbe9cb31850d082d033f38ba8c60105b666f2
Daria Vdovina <vdovinadn@gmail.com>
2023-10-21 20:02:25 +0300

    fix: types

--- src/services/actions/wsActions.ts
+++ src/services/actions/wsActions.ts
@@ -5,8 +5,8 @@ export const wsClose = createAction('WS_CLOSE');
 export const wsConnect = createAction<string>('WS_CONNECT')
 export const wsDisconnect = createAction('WS_DISCONNECT');
 export const wsConnecting = createAction('WS_CONNECTING');
-export const wsMessage = createAction('WS_MESSAGE');
-export const wsError = createAction('WS_ERROR');
+export const wsMessage = createAction<unknown>('WS_MESSAGE');
+export const wsError = createAction<string>('WS_ERROR');
 
 
 export const wsProfileOpen = createAction('WS_PROFILE_OPEN');
@@ -14,5 +14,5 @@ export const wsProfileClose = createAction('WS_PROFILE_CLOSE');
 export const wsProfileConnect = createAction<string>('WS_PROFILE_CONNECT')
 export const wsProfileDisconnect = createAction<string>('WS_PROFILE_DISCONNECT');
 export const wsProfileConnecting = createAction('WS_PROFILE_CONNECTING');
-export const wsProfileMessage = createAction('WS_PROFILE_MESSAGE');
-export const wsProfileError = createAction('WS_PROFILE_ERROR');
+export const wsProfileMessage = createAction<unknown>('WS_PROFILE_MESSAGE');
+export const wsProfileError = createAction<string>('WS_PROFILE_ERROR');
